ccc83cf1daf3f76850e422f1c5409421e14218e3
Qwerty-Art <arty-qwerty@yandex.ru>
2018-09-02 08:07:46 +0300

    dots active

--- build/js/main.js
+++ build/js/main.js
@@ -2,11 +2,13 @@
 // Slider Top
 
 (function () {
-	let prev  = document.querySelector('.slider .prev-botton'),
-			next  = document.querySelector('.slider .next-botton'),
-			slide = document.querySelectorAll('.slider-wrapper .slide'),
-			i = 0;
+	let prev = document.querySelector('.slider .prev-botton'),
+			next = document.querySelector('.slider .next-botton'),
+			slide = document.querySelectorAll('.slider-wrapper .slide');
 	
+
+	// Dots nav
+
 	for (j = 0; j < slide.length; j++){
 		let dot = document.createElement('li'),
 			dots = document.querySelector('.slider .nav-dots');
@@ -16,38 +18,47 @@
 	let dot = document.querySelectorAll('.nav-dots')
 	console.log(dot);
 
-
 	$('.nav-dots li').click(function () {
 		let numberDot = $('.nav-dots li').index(this),
-				slide = $('.slider-wrapper .slide');
-
+			slide = $('.slider-wrapper .slide'),
+			dot = $('.nav-dots li');
+		
+		dot.removeClass();
+		dot.eq(numberDot).addClass('sl-avtive');
 		slide.removeClass('shown');
 		slide.eq(numberDot).addClass('shown');
-
 	})
 
-	// Dots nav
+	function toggleActive() {
 
-	prev.onclick = function () {
-		console.log(slide[i]);
-		$().classList.remove('shown');
-		i = i - 1;
-		if (i < 0) {
-			i = slide.length - 1;
-		};
-		slide[i].classList.add('shown')
 	}
 
 	// Btn Prev Next
 
-	next.onclick = function () {
-		console.log(slide[i]);
-		slide[i].classList.remove('shown');
-		i = i + 1;
-		if (i >= slide.length) {
-			i = 0;
-		};
-		slide[i].classList.add('shown')
+	function findEl() {
+		let el = $('.slider-wrapper .shown'),
+				index = $('.slider-wrapper .slide').index(el);
+		return index;
+		
 	}
-	
+	prev.onclick = function () {
+		let index = findEl();
+		slide[index].classList.remove('shown');
+		index = index - 1;
+		if (index < 0) {
+			index = slide.length - 1;
+		};
+		slide[index].classList.add('shown')
+	}
+
+	next.onclick = function () {	
+		let index = findEl();
+		console.log(slide[index]);
+		slide[index].classList.remove('shown');
+		index = index + 1;
+		if (index >= slide.length) {
+			index = 0;
+		}
+		slide[index].classList.add('shown');
+	}	
 })()
